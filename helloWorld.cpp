8c56411555b086b50d28ffaa6b60017dd52ac656
dmohle <10038913+dmohle@users.noreply.github.com>
2024-08-14 11:53:41 -0700

    Add files via upload
    uploaded my first C++ program

---
+++ helloWorld.cpp
@@ -0,0 +1,16 @@
+// dH 8/14/24
+// Dennis Mohle
+// helloWorld.cpp
+/*
+ * this is my first C++ program
+ * it only took me an hour to write!
+ *
+*/
+#include "iostream"
+using namespace std;
+
+int main() {
+    cout << "\nHello, World! " << "  another string" << endl;
+    return 0;
+}
+// is this the same?
